81ea4be1b340364c12a13e547c88d1141a6411ba
richeekpandey07 <richeekpandey@gmail.com>
2026-07-05 17:49:04 +0530

    Add import statement for sklearn modules

--- employee_retention_analysis.py
+++ employee_retention_analysis.py
@@ -8,6 +8,7 @@ Original file is located at
 """
 import streamlit as st
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
