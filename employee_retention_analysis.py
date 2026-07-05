02cee6c611f66ff0af662bb62b62aaf972024a4c
richeekpandey07 <richeekpandey@gmail.com>
2026-07-05 17:47:54 +0530

    Remove unused matplotlib import
    Removed unused import for matplotlib.

--- employee_retention_analysis.py
+++ employee_retention_analysis.py
@@ -8,7 +8,6 @@ Original file is located at
 """
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
